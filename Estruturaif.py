ab6fbef0afda0fd6f64e6171deea396b0c251ebd
Gabriel Dias <gabriel.amaraldias03@gmail.com>
2023-06-14 17:27:30 -0300

    first commit

--- Estruturaif.py
+++ Estruturaif.py
@@ -3,9 +3,7 @@ l2 = int(input("Valor do segundo lado:"))
 l3 = int(input("Valor do terceiro lado"))
 if l1 == l2 == l3 :
     print(" Esse é um Triângulo    Equilátero")
-elif l1 == l2 != l3 :
-    print("Esse é um Triângulo    Isóscele")
-elif l1 != l2 == l3 :
+elif l1 == l2 != l3 or l1 != l2 == l3  :
     print("Esse é um Triângulo    Isóscele")
 else :
     print("Esse é um Triângulo    Escaleno")
